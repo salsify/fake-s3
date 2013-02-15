0bd5caabc81045c889ee1b268833e137b0935afc
Daniel Spangenberger <dspangenberger@salsify.com>
2013-02-15 09:14:14 -0500

    Fix more bugs around delete and an error message.

--- lib/fakes3/server.rb
+++ lib/fakes3/server.rb
@@ -347,8 +347,6 @@ module FakeS3
         end
 
         if elems.size == 0
-          raise UnsupportedOperation
-        elsif elems.size == 1
           s_req.type = Request::DELETE_BUCKET
           s_req.query = query
         else
